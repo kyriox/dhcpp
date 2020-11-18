d5d914cd0b276ea382b2af78d6efe3de2d5c8bae
Jose Ortiz-Bejar <kyriox@gmail.com>
2020-11-18 10:50:59 -0600

    initial project commit

---
+++ registration/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django import forms
+from django.forms import ModelForm
+from django.utils.safestring import mark_safe
+# Create your models here.
+
+def Person((models.Model):
+        first_name  =  models.CharField(max_length=128, null=False, blank=False)
+        middle_name  =  models.CharField(max_length=128, null=True, blank=True)
+        last_name =  models.CharField(max_length=128, null=False, blank=False)
+        phone_number = models.CharField(max_length=22,  null=True, blank=True)
+        email = models.EmailField(blank=False, null=False, unique=True)
+        gender = models.CharField(max_length=1, choices=(('1','F'),('2','M')))
+           
+        
