4579882260037138893cbb02f54fe57c89de7144
Jose Ortiz-Bejar <kyriox@gmail.com>
2020-11-18 12:11:55 -0600

    modelos basicos

--- registration/admin.py
+++ registration/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from . models import Person, Role
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display=('first_name','last_name','email')
+    search_fields=['last_name','first_name']
+
+class RoleAdmin(admin.ModelAdmin):
+    list_display=('rol_name','description')
 
 # Register your models here.
+admin.site.register(Person,PersonAdmin)
+admin.site.register(Role, RoleAdmin)
--- registration/models.py
+++ registration/models.py
@@ -4,12 +4,21 @@ from django.forms import ModelForm
 from django.utils.safestring import mark_safe
 # Create your models here.
 
-def Person((models.Model):
+class Role(models.Model):
+        rol_name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+        description = models.CharField(max_length=128, null=True, blank=True)
+
+        def __unicode__():
+                return f'{self.rol_name}' 
+
+class Person(models.Model):
         first_name  =  models.CharField(max_length=128, null=False, blank=False)
         middle_name  =  models.CharField(max_length=128, null=True, blank=True)
-        last_name =  models.CharField(max_length=128, null=False, blank=False)
+        last_name = models.CharField(max_length=128, null=False, blank=False)
         phone_number = models.CharField(max_length=22,  null=True, blank=True)
         email = models.EmailField(blank=False, null=False, unique=True)
-        gender = models.CharField(max_length=1, choices=(('1','F'),('2','M')))
+        sex = models.CharField(max_length=1, choices=(('1','F'),('2','M'),('3','O')))
+        role = models.ForeignKey(Role,on_delete=models.CASCADE)
+
            
         
