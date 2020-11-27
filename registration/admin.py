82792dc48dfda71a496cce8d5ef7849465084e40
CristianZavala <a0701308b@fie.umich.mx>
2020-11-27 09:50:17 -0600

    se agregaron modelos

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Person, Role
+from . models import Person, Role, Computer, Localization, MacAdress
 
 class PersonAdmin(admin.ModelAdmin):
     list_display=('first_name','last_name','email')
@@ -8,6 +8,12 @@ class PersonAdmin(admin.ModelAdmin):
 class RoleAdmin(admin.ModelAdmin):
     list_display=('rol_name','description')
 
+class ComputerAdmin(admin.ModelAdmin):
+    list_display=('owner','host_name')
+
 # Register your models here.
 admin.site.register(Person,PersonAdmin)
 admin.site.register(Role, RoleAdmin)
+admin.site.register(Computer, ComputerAdmin)
+admin.site.register(Localization)
+admin.site.register(MacAdress)
--- registration/models.py
+++ registration/models.py
@@ -10,14 +10,47 @@ class Role(models.Model):
         def __unicode__():
                 return f'{self.rol_name}' 
 
+        def __str__(self):
+                return self.rol_name
+
+class Localization(models.Model):
+        name = models.CharField(max_length=128, null=False, blank=False, unique=True)
+        description = models.CharField(max_length=128, null=True, blank=True)
+        def __unicode__():
+                return f'{self.name}' 
+
+        def __str__(self):
+                return self.name
+
+
 class Person(models.Model):
         first_name  =  models.CharField(max_length=128, null=False, blank=False)
         middle_name  =  models.CharField(max_length=128, null=True, blank=True)
         last_name = models.CharField(max_length=128, null=False, blank=False)
         phone_number = models.CharField(max_length=22,  null=True, blank=True)
         email = models.EmailField(blank=False, null=False, unique=True)
-        sex = models.CharField(max_length=1, choices=(('1','F'),('2','M'),('3','O')))
+        registration_number = models.CharField(max_length=22, null=False, blank=False, default=None)
+        #sex = models.CharField(max_length=1, choices=(('1','F'),('2','M'),('3','O')))
         role = models.ForeignKey(Role,on_delete=models.CASCADE)
+
+        def __str__(self):
+                return self.first_name + ' ' + self.last_name
+
+class Computer(models.Model):
+        responsable = models.ForeignKey(Person, related_name="person_responsable", on_delete=models.CASCADE, default=None)
+        owner = models.ForeignKey(Person, related_name="person_owner", on_delete=models.CASCADE)
+        localization = models.ForeignKey(Localization, on_delete=models.CASCADE)
+        host_name = models.CharField(max_length=128, null=False, blank=False)
+        op_sys = models.CharField(max_length=128, null=False, blank=False, default=None)
+        server = models.BooleanField(default=False, verbose_name='It is server?')
+
+        def __str__(self):
+                return self.host_name
+
+class MacAdress(models.Model):
+        mac = models.CharField(max_length = 12, null = False, blank = False)
+        computer = models.OneToOneField(Computer, on_delete=models.CASCADE)
+
 
 class PersonForm(ModelForm):
         class Meta:
